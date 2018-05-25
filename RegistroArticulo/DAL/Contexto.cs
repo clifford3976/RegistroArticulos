ef22adc3ce79c1b9b2cd06070b1ac1f5e1f5992a
CHristelle <CHristelle@DESKTOP-MV2QIEM>
2018-05-25 18:29:03 -0400

    fin de la carpeta DAL

---
+++ RegistroArticulo/DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using RegistroArticulo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace RegistroArticulo.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Articulos> Articulo { get; set; }
+        public Contexto() : base("ConStr")
+        { }
+    }
+}
